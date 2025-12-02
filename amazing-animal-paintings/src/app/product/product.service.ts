3380fcef878e0cee3b2097b1a781652c143c4ff7
Shreya M C <shreya.mc@codecraft.co.in>
2025-12-02 18:20:09 +0530

    feat: create environment and get products from service

--- amazing-animal-paintings/src/app/product/product.service.ts
+++ amazing-animal-paintings/src/app/product/product.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../model/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private apiUrl = environment.apiUrl + '/products';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
 }
