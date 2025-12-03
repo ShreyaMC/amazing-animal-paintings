ed1da9fcd0ffa35a3ab470cb6e47614134f8bd58
Shreya M C <shreya.mc@codecraft.co.in>
2025-12-04 00:20:12 +0530

    feat: add navbar, create card module, service and component

--- amazing-animal-paintings/src/app/app.module.ts
+++ amazing-animal-paintings/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductModule } from './product/product.module';
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ProductModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ProductModule,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
