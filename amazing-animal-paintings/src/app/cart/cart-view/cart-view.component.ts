b3df781a5c96dab7e9d213dde1b8fe43b223aa03
Shreya M C <shreya.mc@codecraft.co.in>
2025-12-04 15:40:45 +0530

    feat: add total price, clear and checkout button

--- amazing-animal-paintings/src/app/cart/cart-view/cart-view.component.ts
+++ amazing-animal-paintings/src/app/cart/cart-view/cart-view.component.ts
@@ -10,12 +10,30 @@ import { Product } from 'src/app/model/product';
 })
 export class CartViewComponent implements OnInit {
   cartItems: Product[] = [];
+  totalPrice: number = 0;
 
   constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
     this.cartService.getCartItems().subscribe((data) => {
       this.cartItems = data;
+      this.totalPrice = this.getTotalPrice();
     });
+  }
+
+  getTotalPrice() {
+    let total = 0;
+    for (let item of this.cartItems) {
+      total += item.price;
+    }
+    return total;
+  }
+
+  clearCart(): void {
+    this.cartService.clearCart().subscribe();
+  }
+
+  checkout(): void {
+    this.cartService.checkout(this.cartItems).subscribe();
   }
 }
